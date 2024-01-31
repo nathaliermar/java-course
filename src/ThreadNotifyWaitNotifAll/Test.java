fc51acdf2df8ae2c7ca4c5b7c4359f2d47b8cf2f
Nathalie Martines <110064258+nathaliermar@users.noreply.github.com>
2024-01-31 00:35:03 -0300

    Update Test.java

--- src/ThreadNotifyWaitNotifAll/Test.java
+++ src/ThreadNotifyWaitNotifAll/Test.java
@@ -1,2 +1,18 @@
-package ThreadNotifyWaitNotifAll;public class Test {
+package ThreadNotifyWaitNotifAll;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        TickTock tt = new TickTock();
+        ThreadTickTock tick = new ThreadTickTock("Tick", tt);
+        ThreadTickTock tack = new ThreadTickTock("Tack", tt);
+
+        try {
+            tick.t.join();
+            tack.t.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
