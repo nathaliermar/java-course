16b5c580684d3f69eb3befa8effe9ebda568b26c
Nathalie Martines <110064258+nathaliermar@users.noreply.github.com>
2024-01-31 00:30:30 -0300

    Update Tokenizer.java

--- src/Strings/Tokenizer.java
+++ src/Strings/Tokenizer.java
@@ -1,2 +1,20 @@
-package Strings;public class Tokenizer {
+package Strings;
+
+import java.util.StringTokenizer;
+
+//Extracting tokens from the string we are extracting information from.
+//Two methods -> while and nextToken (extraction info from tokenizer).
+
+public class Tokenizer {
+
+    public static void main(String[] args) {
+
+        String fromFile = "1;Matheus;21;";
+
+        StringTokenizer st = new StringTokenizer(fromFile, ";");
+
+        while (st.hasMoreTokens()) {
+            System.out.println(st.nextToken());
+        }
+    }
 }
