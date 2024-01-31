ba6a86d5b56074437ad7c1c86fbc1a73c7adde5a
Nathalie Martines <110064258+nathaliermar@users.noreply.github.com>
2024-01-31 00:16:05 -0300

    Update Person.java

--- src/Enum/Person.java
+++ src/Enum/Person.java
@@ -1,2 +1,41 @@
-package Enum;public class Person {
+package Enum;
+
+public class Person {
+
+    private DocumentType documentType;
+    private String numberDocument;
+
+
+    public Person(){
+        super();
+    }
+
+    public Person(DocumentType documentType, String numberDocument) {
+        this.documentType = documentType;
+        this.numberDocument = numberDocument;
+    }
+
+    public DocumentType getDocumentType() {
+        return documentType;
+    }
+
+    public void setDocumentType(DocumentType documentType) {
+        this.documentType = documentType;
+    }
+
+    public String getNumberDocument() {
+        return numberDocument;
+    }
+
+    public void setNumberDocument(String numberDocument) {
+        this.numberDocument = numberDocument;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "documentType=" + documentType +
+                ", numberDocument='" + numberDocument + '\'' +
+                '}';
+    }
 }
