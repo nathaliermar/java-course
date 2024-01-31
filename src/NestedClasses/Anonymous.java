e40f48e7089ee9b1ed5154ea797fb5a0f2d915ca
Nathalie Martines <110064258+nathaliermar@users.noreply.github.com>
2024-01-31 00:20:52 -0300

    Update Anonymous.java

--- src/NestedClasses/Anonymous.java
+++ src/NestedClasses/Anonymous.java
@@ -1,2 +1,30 @@
-package NestedClasses;public class Anonymous {
+package NestedClasses;
+
+//Can implement interfaces
+
+public class Anonymous {
+
+    public void printText() {
+        System.out.println("Any text");
+    }
+
+    public static void main(String[] args) {
+
+        Anonymous anonymous = new Anonymous() {
+            public void printText() {
+                System.out.println("Any text that has been overwritten");
+            }
+        };
+
+        anonymous.printText();
+
+        Text text = new Text() {
+            @Override
+            public void printText() {
+                System.out.println("Any text - interface");
+            }
+        };
+
+        text.printText();
+    }
 }
