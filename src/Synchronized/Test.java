fbe92b0320931edc85a0bff94f843252079eefcd
Nathalie Martines <110064258+nathaliermar@users.noreply.github.com>
2024-01-31 00:34:40 -0300

    Update Test.java

--- src/Synchronized/Test.java
+++ src/Synchronized/Test.java
@@ -1,2 +1,11 @@
-package Synchronized;public class Test {
+package Synchronized;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        int[] array = {1, 2, 3, 4, 5};
+        MyThreadSum t1 = new MyThreadSum("#1", array);
+        MyThreadSum t2 = new MyThreadSum("#2", array);
+    }
 }
