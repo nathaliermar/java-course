24a3e931b3620bb0dec6c1a0cd851bf8d42e98e9
Nathalie Martines <110064258+nathaliermar@users.noreply.github.com>
2024-01-31 00:34:27 -0300

    Update MyThreadSum.java

--- src/Synchronized/MyThreadSum.java
+++ src/Synchronized/MyThreadSum.java
@@ -1,2 +1,32 @@
-package Synchronized;public class MyThreadSum {
+package Synchronized;
+
+public class MyThreadSum implements Runnable {
+
+    private String name;
+    private int[] numbers;
+    private static Calculator cal = new Calculator();
+
+    public MyThreadSum(String name, int[] numbers) {
+        this.name = name;
+        this.numbers = numbers;
+
+        new Thread(this, name).start();
+
+        //Thread t = new Thread(this, name);
+        //t.start();
+
+    }
+
+
+    @Override
+    public void run() {
+
+        System.out.println(this.name + " started");
+
+        int sum = cal.sumArray(this.numbers);
+
+        System.out.println("Result sum for thread " + this.name + " is: " + sum);
+
+        System.out.println(this.name + " finished");
+    }
 }
