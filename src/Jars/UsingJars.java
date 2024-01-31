5c250daf981fb440c4d3f3050a1349ca462e0d61
Nathalie Martines <110064258+nathaliermar@users.noreply.github.com>
2024-01-31 00:18:26 -0300

    Update UsingJars.java

--- src/Jars/UsingJars.java
+++ src/Jars/UsingJars.java
@@ -1,2 +1,13 @@
-package Jars;public class UsingJars {
+package Jars;
+
+import org.apache.commons.lang.StringUtils;
+
+public class UsingJars {
+
+    public static void main(String[] args) {
+
+        // StringUtils - several methods that we can use with it, we can see all of them in the Jar documentation.
+
+        System.out.println(StringUtils.capitalize("nathalie"));
+    }
 }
