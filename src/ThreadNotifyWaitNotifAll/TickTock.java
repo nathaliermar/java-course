89bf4b5db2d6a32604365ccb6f6e85f1a011ddb6
Nathalie Martines <110064258+nathaliermar@users.noreply.github.com>
2024-01-31 00:35:32 -0300

    Update TickTock.java

--- src/ThreadNotifyWaitNotifAll/TickTock.java
+++ src/ThreadNotifyWaitNotifAll/TickTock.java
@@ -1,2 +1,52 @@
-package ThreadNotifyWaitNotifAll;public class TickTock {
+package ThreadNotifyWaitNotifAll;
+
+public class TickTock {
+
+    boolean tick;
+
+    synchronized void tick(boolean isRunning) {
+
+        if (!isRunning) {
+            tick = true;
+            notify();
+            return;
+        }
+
+        System.out.println("Tick ");
+
+        tick = true;
+
+        notify();
+
+        try {
+            while (tick) {
+                wait();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    synchronized void tock(boolean isRunning) {
+
+        if (!isRunning) {
+            tick = false;
+            notify();
+            return;
+        }
+
+        System.out.println("Tock ");
+
+        tick = false;
+
+        notify();
+
+        try {
+            while (!tick) {
+                wait();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
