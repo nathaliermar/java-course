34ec7a7bb9fb3099db2c28b91369106e2aa3fe57
Nathalie Martines <110064258+nathaliermar@users.noreply.github.com>
2024-01-31 00:16:49 -0300

    Update TestEnum.java

--- src/Enum/TestEnum.java
+++ src/Enum/TestEnum.java
@@ -1,2 +1,17 @@
-package Enum;public class TestEnum {
+package Enum;
+
+public class TestEnum {
+
+  public static void main(String[] args) {
+
+        DayWeek[] days = DayWeek.values();
+
+        for (int i = 0; i < days.length; i++) {
+            System.out.println(days[i]);
+        }
+
+        for (DayWeek day : DayWeek.values()){
+            System.out.println(day);
+        }
+    }
 }
