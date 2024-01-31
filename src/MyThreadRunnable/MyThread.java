461f60df70c538347f789aad5108a6970ab8b80c
Nathalie Martines <110064258+nathaliermar@users.noreply.github.com>
2024-01-31 00:18:47 -0300

    Update MyThread.java

--- src/MyThreadRunnable/MyThread.java
+++ src/MyThreadRunnable/MyThread.java
@@ -1,2 +1,28 @@
-package MyThreadRunnable;public class MyThread {
+package MyThreadRunnable;
+
+public class MyThread implements Runnable {
+
+    private String name;
+    private int time;
+
+    public MyThread(String name, int time) {
+        this.name = name;
+        this.time = time;
+//        Thread t = new Thread(this);
+//        t.start();
+    }
+
+    @Override
+    public void run() {
+
+        try {
+            for (int i=0; i<6; i++) {
+                System.out.println(name + " counter " + i);
+                Thread.sleep(time);
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(name + " finished execution");
+    }
 }
