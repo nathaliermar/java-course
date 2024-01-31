adef7599b89a0437e2d0a047cf4fcf636da99018
Nathalie Martines <110064258+nathaliermar@users.noreply.github.com>
2024-01-31 00:25:37 -0300

    Update TestStaticImport.java

--- src/StaticImport/TestStaticImport.java
+++ src/StaticImport/TestStaticImport.java
@@ -1,2 +1,18 @@
-package StaticImport;public class TestStaticImport {
+package StaticImport;
+
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
+
+public class TestStaticImport {
+
+    public static void main(String[] args) {
+
+        double a = 2;
+        double b = 3;
+        double c = 4;
+
+        System.out.println(pow(a, b));
+
+        System.out.println(sqrt(c));
+    }
 }
