a0a02c80e50a12dad9aadd764b961f462d51bc67
Nathalie Martines <110064258+nathaliermar@users.noreply.github.com>
2024-01-31 00:32:47 -0300

    Update Index.java

--- src/StringsComparation/Index.java
+++ src/StringsComparation/Index.java
@@ -1,2 +1,26 @@
-package StringsComparation;public class Index {
+package StringsComparation;
+
+// -1 when doesn't exist
+
+public class Index {
+
+    public static void main(String[] args) {
+
+        String banana = "banana";
+        String ana = "ana";
+
+        System.out.println(banana.indexOf('x'));
+        System.out.println(banana.indexOf('b'));
+        System.out.println(banana.indexOf('a'));
+
+        System.out.println(banana.indexOf(ana));
+
+       //Last time the lyric or phrase existed
+        System.out.println(banana.lastIndexOf('a'));
+        System.out.println(banana.lastIndexOf(ana));
+
+
+        //Returns true or false if it exists in the sentence
+        System.out.println(banana.contains(ana));
+    }
 }
