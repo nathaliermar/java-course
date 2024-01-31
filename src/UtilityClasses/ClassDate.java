ecbc2f24c071869b4a263b6ce3a03d20534c6f9a
Nathalie Martines <110064258+nathaliermar@users.noreply.github.com>
2024-01-31 00:37:43 -0300

    Update ClassDate.java

--- src/UtilityClasses/ClassDate.java
+++ src/UtilityClasses/ClassDate.java
@@ -2,13 +2,17 @@ package UtilityClasses;
 
 import java.util.Date;
 
-public class UtilityDate {
+public class ClassDate {
 
     public static void main(String[] args) {
 
         Date today = new Date();
 
         System.out.println(today);
+
+        System.out.println("Milliseconds since January 1, 1970 is " + today.getTime());
+
+        System.out.println(today.getDate());
 
     }
 }
