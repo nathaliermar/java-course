ee1d231861673f265a4058092c6781edde898ce1
Nathalie Martines <110064258+nathaliermar@users.noreply.github.com>
2024-01-31 00:34:12 -0300

    Update Calculator.java

--- src/Synchronized/Calculator.java
+++ src/Synchronized/Calculator.java
@@ -1,2 +1,27 @@
-package Synchronized;public class Calculator {
+package Synchronized;
+
+public class Calculator {
+
+    private int sum;
+
+    public synchronized int sumArray(int[] array) {
+
+        sum = 0;
+
+        for (int i=0; i<array.length; i++) {
+
+            sum += array[i];
+
+            System.out.println("Running the sum " + Thread.currentThread().getName() + " adding the value " +
+                    array[i] + " with a total of " + sum);
+
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return sum;
+    }
 }
