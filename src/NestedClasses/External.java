f8aa4d256b22fa6f03a50c05561e565120b51b4d
Nathalie Martines <110064258+nathaliermar@users.noreply.github.com>
2024-01-31 00:21:17 -0300

    Update External.java

--- src/NestedClasses/External.java
+++ src/NestedClasses/External.java
@@ -1,7 +1,27 @@
 package NestedClasses;
 
 //static or not
-public class Extern {
-    private String text = "text extern"
+
+public class External {
+    private String text = "External text ";
+
+    public class Internal{
+
+        private String text = "Internal text";
+
+        public void printText(){
+            System.out.println(text);
+
+            System.out.println(External.this.text);
+        }
+    }
+
+    public static void main(String[] args){
+
+        External external = new External();
+        Internal internal = external.new Internal();
+
+        internal.printText();
+    }
 
 }
