3444c3f8143d8f60bfb015d272494cc41db2d346
Nathalie Martines <110064258+nathaliermar@users.noreply.github.com>
2024-01-31 00:35:18 -0300

    Update ThreadTickTock.java

--- src/ThreadNotifyWaitNotifAll/ThreadTickTock.java
+++ src/ThreadNotifyWaitNotifAll/ThreadTickTock.java
@@ -1,2 +1,31 @@
-package ThreadNotifyWaitNotifAll;public class ThreadTickTock {
+package ThreadNotifyWaitNotifAll;
+
+public class ThreadTickTock implements Runnable {
+
+    TickTock tt;
+    Thread t;
+
+    final int NUM = 5;
+
+    public ThreadTickTock(String name, TickTock tt) {
+        this.tt = tt;
+        t = new Thread(this, name);
+        t.start();
+    }
+
+    @Override
+    public void run() {
+
+        if (t.getName().equalsIgnoreCase("Tick")){
+            for (int i=0; i<NUM; i++) {
+                tt.tick(true);
+            }
+            tt.tick(false);
+        } else {
+            for (int i=0; i<NUM; i++) {
+                tt.tock(true);
+            }
+            tt.tock(false);
+        }
+    }
 }
