04510c3c5ec584b85c210fea3a5cad6d835a9dcc
Nathalie Martines <110064258+nathaliermar@users.noreply.github.com>
2024-01-31 00:17:06 -0300

    Update TestEnum2.java

--- src/Enum/TestEnum2.java
+++ src/Enum/TestEnum2.java
@@ -1,2 +1,13 @@
-package Enum;public class TestEnum2 {
+package Enum;
+
+public class TestEnum2 {
+
+  public static void main(String[] args) {
+
+        System.out.println(Enum.valueOf(DayWeek.class, "MONDAY"));
+
+        DayWeek day = Enum.valueOf(DayWeek.class, "MONDAY");
+
+        System.out.println(day);
+    }
 }
