e151a0518d102a0dd89fb44aae7ff64be3312d67
Nathalie Martines <110064258+nathaliermar@users.noreply.github.com>
2024-01-31 00:24:37 -0300

    Update Contact.java

--- src/PassingParameters/Contact.java
+++ src/PassingParameters/Contact.java
@@ -1,2 +1,47 @@
-package PassingParameters;public class Contact {
+package PassingParameters;
+
+public class Contact {
+
+        private String name;
+        private String cellphone;
+        private String email;
+
+    public Contact(String name, String cellphone, String email) {
+        this.name = name;
+        this.cellphone = cellphone;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCellphone() {
+        return cellphone;
+    }
+
+    public void setCellphone(String cellphone) {
+        this.cellphone = cellphone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "name='" + name + '\'' +
+                ", cellphone='" + cellphone + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
