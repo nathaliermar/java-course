fef2d8dae5732e3e0276faafc18f850b42d76dbc
Nathalie Martines <110064258+nathaliermar@users.noreply.github.com>
2024-01-31 00:24:17 -0300

    Update ClassRandom.java

--- src/Numbers/ClassRandom.java
+++ src/Numbers/ClassRandom.java
@@ -1,2 +1,15 @@
-package Numbers;public class ClassRandom {
+package Numbers;
+
+import java.util.Random;
+
+public class ClassRandom {
+
+    public static void main(String[] args) {
+
+        System.out.println(Math.floor(Math.random() * 10));
+
+        Random random = new Random();
+
+        System.out.println(random.nextInt(2 + 2));
+    }
 }
