476de1f74c03f422f116a3d5346e055d9edc6c8a
Nathalie Martines <110064258+nathaliermar@users.noreply.github.com>
2024-01-31 00:22:01 -0300

    Update External2.java

--- src/NestedClasses/External2.java
+++ src/NestedClasses/External2.java
@@ -1,5 +1,27 @@
 package NestedClasses;
 
-public class Local {
+public class External2 {
 
+     public void anyMethod() {
+
+         class LocalClass{
+
+             private String text = "local class text";
+
+             public void printText() {
+                 System.out.println(text);
+             }
+         }
+
+         LocalClass local = new LocalClass();
+
+         local.printText();
+     }
+
+    public static void main(String[] args) {
+
+         External2 external = new External2();
+
+         external.anyMethod();
+    }
 }
